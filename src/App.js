a4f566d02ef8e38131e59187cd95d1e6a7a81380
youssefThiri <tahiri.youssef.ken@gmail.com>
2023-12-08 19:37:34 +0100

    adding header component and links to home , fhop page and contact

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Switch , Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component.jsx' ;
 
 
 function App () {
   return (
     <div>
-      <Switch>
+      <Header/>
+       <Switch>
           <Route exact path = '/' component = {HomePage } /> 
           <Route path = '/shop' component= {ShopPage} />   
       </Switch>
